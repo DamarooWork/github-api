7f15a230cd1a55c5a5ba06797c2d61a48f225812
Damaroo <levkichigin@yandex.ru>
2025-02-20 11:23:32 +0300

    store

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -1,10 +1,10 @@
 'use client'
 import { useRef } from 'react'
 import { Provider } from 'react-redux'
-import { makeStore, AppStore } from '../lib/store'
+import { makeStore, AppStore } from '../store'
 
 export default function StoreProvider({
-  children
+  children,
 }: {
   children: React.ReactNode
 }) {
