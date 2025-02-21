5e28f17a22411e034745cea64cabc15d9f57ba6b
Damaroo <levkichigin@yandex.ru>
2025-02-21 11:39:56 +0300

    fixes

--- src/components/favorites/index.tsx
+++ src/components/favorites/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
     <section className="mt-4 text-2xl">
       {favourites?.length ? (
         <ul className="flex flex-col gap-4  ">
-          {favourites.map((fav, i) => (
+          {favourites.map((fav) => (
             <Favorite key={fav} fav={fav} />
           ))}
         </ul>
